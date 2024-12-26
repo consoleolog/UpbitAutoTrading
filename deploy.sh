e669ca3e68711dcc38f264be676d02bfce12a4b1
consolelog <leopportunity5@gmail.com>
2024-12-26 09:49:42 +0900

    2024-12-26 :: modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,20 +4,20 @@ EXIST_BLUE=$(sudo docker-compose -p autotrading-blue -f docker-compose.blue.yaml
 
 if [ -z "$EXIST_BLUE" ]; then
   echo "blue up"
-  docker-compose -p autotrading-blue -f docker-compose.blue.yaml up -d --build
+  sudo docker-compose -p autotrading-blue -f docker-compose.blue.yaml up -d --build
   BEFORE_COMPOSE_COLOR="green"
   AFTER_COMPOSE_COLOR="blue"
 else
   echo "green up"
-  docker-compose -p autotrading-green -f docker-compose.green.yaml up -d --build
+  sudo docker-compose -p autotrading-green -f docker-compose.green.yaml up -d --build
   BEFORE_COMPOSE_COLOR="blue"
   AFTER_COMPOSE_COLOR="green"
 fi
 
 sleep 10
 
-EXIST_AFTER=$(docker-compose -p autotrading-${AFTER_COMPOSE_COLOR} -f docker-compose.${AFTER_COMPOSE_COLOR}.yaml ps | grep Up)
+EXIST_AFTER=$(sudo docker-compose -p autotrading-${AFTER_COMPOSE_COLOR} -f docker-compose.${AFTER_COMPOSE_COLOR}.yaml ps | grep Up)
 if [ -n "$EXIST_AFTER" ]; then
-  docker-compose -p autotrading-${BEFORE_COMPOSE_COLOR} -f docker-compose.${BEFORE_COMPOSE_COLOR}.yaml down
+  sudo docker-compose -p autotrading-${BEFORE_COMPOSE_COLOR} -f docker-compose.${BEFORE_COMPOSE_COLOR}.yaml down
   echo "$BEFORE_COMPOSE_COLOR down"
 fi
