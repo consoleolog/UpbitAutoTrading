c3d6a187632f897c21948af67e5257c6d3977897
consolelog <leopportunity5@gmail.com>
2024-12-26 09:47:25 +0900

    2024-12-26 :: update

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-EXIST_BLUE=$(sudo docker-compose -p "${DOCKER_APP_NAME}"-blue -f docker-compose.blue.yaml ps | grep Up)
+EXIST_BLUE=$(sudo docker-compose -p autotrading-blue -f docker-compose.blue.yaml ps | grep Up)
 
 if [ -z "$EXIST_BLUE" ]; then
   echo "blue up"
-  docker-compose -p "${DOCKER_APP_NAME}"-blue -f docker-compose.blue.yaml up -d
+  docker-compose -p autotrading-blue -f docker-compose.blue.yaml up -d --build
   BEFORE_COMPOSE_COLOR="green"
   AFTER_COMPOSE_COLOR="blue"
 else
   echo "green up"
-  docker-compose -p "${DOCKER_APP_NAME}"-green -f docker-compose.green.yaml up -d
+  docker-compose -p autotrading-green -f docker-compose.green.yaml up -d --build
   BEFORE_COMPOSE_COLOR="blue"
   AFTER_COMPOSE_COLOR="green"
 fi
 
 sleep 10
 
-EXIST_AFTER=$(docker-compose -p "${DOCKER_APP_NAME}"-${AFTER_COMPOSE_COLOR} -f docker-compose.${AFTER_COMPOSE_COLOR}.yaml ps | grep Up)
+EXIST_AFTER=$(docker-compose -p autotrading-${AFTER_COMPOSE_COLOR} -f docker-compose.${AFTER_COMPOSE_COLOR}.yaml ps | grep Up)
 if [ -n "$EXIST_AFTER" ]; then
-  docker-compose -p "${DOCKER_APP_NAME}"-${BEFORE_COMPOSE_COLOR} -f docker-compose.${BEFORE_COMPOSE_COLOR}.yaml down
+  docker-compose -p autotrading-${BEFORE_COMPOSE_COLOR} -f docker-compose.${BEFORE_COMPOSE_COLOR}.yaml down
   echo "$BEFORE_COMPOSE_COLOR down"
 fi
-
