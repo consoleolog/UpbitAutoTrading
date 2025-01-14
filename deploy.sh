2a5e8b58ec8b48f1165c3d2fa81452e41139eec1
consolelog <leopportunity5@gmail.com>
2025-01-14 17:43:35 +0900

    2025-01-14 :: update

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-EXIST_BLUE=$(sudo docker-compose -p autotrading-blue -f docker-compose.blue.yaml ps | grep Up)
+IMAGE_NAME="upbit_trading:V1"
+CONTAINER_NAME="autotrading"
+
+EXIST_BLUE=$(sudo docker-compose -p $CONTAINER_NAME-blue -f docker-compose.blue.yaml ps | grep Up)
+IMAGE_ID=$(sudo docker -q $IMAGE_NAME)
+
+sudo docker rmi "$IMAGE_ID"
 
 if [ -z "$EXIST_BLUE" ]; then
   echo "blue up"
-  sudo docker-compose -p autotrading-blue -f docker-compose.blue.yaml up -d --build
+  sudo docker-compose -p $CONTAINER_NAME-blue -f docker-compose.blue.yaml up -d --build
   BEFORE_COMPOSE_COLOR="green"
   AFTER_COMPOSE_COLOR="blue"
 else
   echo "green up"
-  sudo docker-compose -p autotrading-green -f docker-compose.green.yaml up -d --build
+  sudo docker-compose -p $CONTAINER_NAME-green -f docker-compose.green.yaml up -d --build
   BEFORE_COMPOSE_COLOR="blue"
   AFTER_COMPOSE_COLOR="green"
 fi
