040ef668306f10ad2d5398adc96ae471940eab7d
consolelog <leopportunity5@gmail.com>
2025-01-16 15:56:28 +0900

    2025-01-16 :: remove error log

--- job_factory.py
+++ job_factory.py
@@ -36,57 +36,49 @@ class JobFactory:
              ema: EMA = EMA(),
              candle_request_dto: CandleRequestDto = CandleRequestDto(),
              ):
-        try:
-            candle_service = CandleService(
-                ema=ema,
-                candle_data_repository=self.candle_data_repository,
-                upbit_module=self.upbit_module
-            )
 
-            data = candle_service.get_candle_data(candle_request_dto)
+        candle_service = CandleService(
+            ema=ema,
+            candle_data_repository=self.candle_data_repository,
+            upbit_module=self.upbit_module
+        )
 
-            stage = data_util.get_stage_from_ema(data=data)
+        data = candle_service.get_candle_data(candle_request_dto)
 
-            candle_data = CandleData(
-                ticker=candle_request_dto.ticker,
-                close=float(data[CandleResponseDto.CLOSE].iloc[-1]),
-                ema_short=float(data[EMA.SHORT].iloc[-1]),
-                ema_middle=float(data[EMA.MIDDLE].iloc[-1]),
-                ema_long=float(data[EMA.LONG].iloc[-1]),
-                stage=stage,
-                macd_upper=float(data[MACD.UPPER].iloc[-1]),
-                macd_middle=float(data[MACD.MIDDLE].iloc[-1]),
-                macd_lower=float(data[MACD.LOWER].iloc[-1]),
-                interval=candle_request_dto.interval,
-            )
+        stage = data_util.get_stage_from_ema(data=data)
 
-            candle_service.save_data(candle_data=candle_data)
+        candle_data = CandleData(
+            ticker=candle_request_dto.ticker,
+            close=float(data[CandleResponseDto.CLOSE].iloc[-1]),
+            ema_short=float(data[EMA.SHORT].iloc[-1]),
+            ema_middle=float(data[EMA.MIDDLE].iloc[-1]),
+            ema_long=float(data[EMA.LONG].iloc[-1]),
+            stage=stage,
+            macd_upper=float(data[MACD.UPPER].iloc[-1]),
+            macd_middle=float(data[MACD.MIDDLE].iloc[-1]),
+            macd_lower=float(data[MACD.LOWER].iloc[-1]),
+            interval=candle_request_dto.interval,
+        )
 
-            order_request_dto = self.order_service.create_order_request_dto(
-                candle_request_dto=candle_request_dto,
-                data=data,
-                stage=stage
-            )
-            if order_request_dto is not None:
-                # 매수 신호
-                if order_request_dto.price is not None:
-                    order_response_dto = self.order_service.buy_market_order(order_request_dto)
+        candle_service.save_data(candle_data=candle_data)
+
+        order_request_dto = self.order_service.create_order_request_dto(
+            candle_request_dto=candle_request_dto,
+            data=data,
+            stage=stage
+        )
+        if order_request_dto is not None:
+            # 매수 신호
+            if order_request_dto.price is not None:
+                order_response_dto = self.order_service.buy_market_order(order_request_dto)
+                self.order_service.save_data(order_response_dto)
+
+            # 매도 신호
+            elif order_request_dto.volume is not None:
+                is_profit = self.order_service.is_profit(candle_request_dto.ticker)
+                if is_profit:
+                    order_response_dto = self.order_service.sell_market_order(order_request_dto)
                     self.order_service.save_data(order_response_dto)
 
-                # 매도 신호
-                elif order_request_dto.volume is not None:
 
-                    is_profit = self.order_service.is_profit(candle_request_dto.ticker)
-                    if is_profit:
-                        order_response_dto = self.order_service.sell_market_order(order_request_dto)
-                        self.order_service.save_data(order_response_dto)
-
-        except Exception as err:
-            self.logger.warn(f"""
-            =======================
-                     ERROR
-                ticker : {candle_request_dto.ticker}
-                err: {str(err)}
-            =======================
-            """)
 
