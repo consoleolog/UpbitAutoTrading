1cdce1a50cb91a222b062f423fd550c092ff7a9e
fergus <leopportunity5@gmail.com>
2025-02-22 07:30:50 +0900

    fix: small

--- trade.py
+++ trade.py
@@ -23,19 +23,21 @@ def execute(ticker, timeframe: TimeFrame):
 
     if balance == 0:
         peekout = all([
-            data[MACD.SHORT_HIST].iloc[-1] <= 0,
-            data[MACD.MID_HIST].iloc[-1] <= 0,
-            data[MACD.LONG_HIST].iloc[-1] <= 0,
             data[MACD.SHORT_HIST].iloc[-1] > data[MACD.SHORT_HIST].iloc[-7:].min(),
             data[MACD.MID_HIST].iloc[-1] > data[MACD.MID_HIST].iloc[-7:].min(),
             data[MACD.LONG_HIST].iloc[-1] > data[MACD.LONG_HIST].iloc[-7:].min() 
         ])
-        if peekout and rsi <= 45 and stage in [Stage.STABLE_DECREASE, Stage.END_OF_DECREASE, Stage.START_OF_INCREASE]:
+        bullish = all([
+            data[MACD.SHORT_BULLISH].iloc[-2:].isin([True]).any(),
+            data[MACD.MID_BULLISH].iloc[-2:].isin([True]).any(),
+            data[MACD.LONG_BULLISH].iloc[-2:].isin([True]).any(),
+        ])
+        if bullish and rsi <= 40 and stage in [Stage.STABLE_DECREASE, Stage.END_OF_DECREASE, Stage.START_OF_INCREASE]:
             mapper.insert_order(ticker, exchange.get_current_price(ticker), "bid")
             exchange.create_buy_order(ticker, 20000)
         info["data"] = f"[MACD: {peekout} | RSI: {rsi}]"
     else:
-        profit = calculate_profit(ticker, float(data["close"].iloc[-1]))
+        profit = calculate_profit(ticker, exchange.get_current_price(ticker))
         if profit > 0.1:
             mapper.insert_order(ticker, exchange.get_current_price(ticker), "ask")
             exchange.create_sell_order(ticker, balance)
