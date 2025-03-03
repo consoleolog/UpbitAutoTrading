373a26f868e370d526f0876d459b997de5f7041a
fergus <handsomefergus04@gmail.com>
2025-03-03 13:52:29 +0900

    fix: except rsi in condition

--- trade.py
+++ trade.py
@@ -44,9 +44,8 @@ def execute(ticker, timeframe: TimeFrame):
         profit = calculate_profit(ticker, exchange.get_current_price(ticker))
         stoch_bearish = data[STOCHASTIC.BEARISH].iloc[-2:].isin([True]).any()
         macd_bearish = data[MACD.LONG_BEARISH].iloc[-2:].isin([True]).any() or data[MACD.SHORT_BEARISH].iloc[-2:].isin([True]).any()
-        rsi_bearish = data[RSI.LONG_BEARISH].iloc[-2:].isin([True]).any()
         info["profit"] = profit
-        if profit < 0 and (stoch_bearish or macd_bearish or rsi_bearish) and stage == Stage.STABLE_INCREASE:
+        if profit < 0 and (stoch_bearish or macd_bearish) and stage == Stage.STABLE_INCREASE:
             mapper.update_status(ticker, exchange.get_current_price(ticker), "ask")
             exchange.create_sell_order(ticker, balance)
             return info
@@ -54,7 +53,7 @@ def execute(ticker, timeframe: TimeFrame):
             mapper.update_status(ticker, exchange.get_current_price(ticker), "ask")
             exchange.create_sell_order(ticker, balance)
             return info
-        if profit > 0.1 and (stoch_bearish or macd_bearish or rsi_bearish):
+        if profit > 0.1 and (stoch_bearish or macd_bearish):
             mapper.update_status(ticker, exchange.get_current_price(ticker), "ask")
             exchange.create_sell_order(ticker, balance)
             return info
