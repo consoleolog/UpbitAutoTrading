a9439eb921203d73f8244b3adb2fa41fc11c3df3
fergus <leopportunity5@gmail.com>
2025-03-04 23:44:39 +0900

    fix: small

--- trade.py
+++ trade.py
@@ -35,7 +35,7 @@ def execute(ticker, timeframe: TimeFrame):
         data[MACD.LONG_BULLISH].iloc[-2:].isin([True]).any(),
     ])
     info["data"] = f"[MACD: {bullish} | RSI: {rsi}]"
-    if bullish and rsi <= 40 and exchange.get_krw() > 20000 and stage in [Stage.STABLE_DECREASE, Stage.END_OF_DECREASE, Stage.START_OF_INCREASE]:
+    if bullish and (rsi <= 40 or rsi > 50) and exchange.get_krw() > 20000 and stage in [Stage.STABLE_DECREASE, Stage.END_OF_DECREASE, Stage.START_OF_INCREASE]:
         exchange.create_buy_order(ticker, 20000)
         update_status(ticker)
         return info
